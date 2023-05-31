f71aece28842aa9f0eadb4f4af378c9424d10147
rajasekhar-ganji <131284665+rajasekhar-ganji@users.noreply.github.com>
2023-05-31 11:12:54 +0530

    Update ConstantPathsAndvalues.java

--- ApiTestingPetSwagger/src/main/java/utils/ConstantPathsAndvalues.java
+++ ApiTestingPetSwagger/src/main/java/utils/ConstantPathsAndvalues.java
@@ -6,6 +6,6 @@ public class ConstantPathsAndvalues {
 	 public final static String propertyfile_path  = PROJECT_BASE_PATH + "/src/test/resources/PetSwaggerProperties.properties";
 	 public final static String petpost = PROJECT_BASE_PATH + "/src/test/resources/petadd.json";
 	    public final static String updatepet = PROJECT_BASE_PATH + "/src/test/resources/updatepet.json";
-	    public final static String uploadpic = PROJECT_BASE_PATH + "/src/test/resources/puppy.jpg";
+	    public final static String uploadimage = PROJECT_BASE_PATH + "/src/test/resources/puppy.jpg";
 	    public final static int statuscode=200;
 }
