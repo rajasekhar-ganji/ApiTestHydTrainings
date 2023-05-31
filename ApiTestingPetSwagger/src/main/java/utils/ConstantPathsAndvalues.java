290f4f484834154021fe1ff7687d9bd63530bde6
rajasekhar-ganji <131284665+rajasekhar-ganji@users.noreply.github.com>
2023-05-31 11:52:05 +0530

    Update ConstantPathsAndvalues.java

--- ApiTestingPetSwagger/src/main/java/utils/ConstantPathsAndvalues.java
+++ ApiTestingPetSwagger/src/main/java/utils/ConstantPathsAndvalues.java
@@ -3,9 +3,11 @@ package utils;
 public class ConstantPathsAndvalues {
 
 	public final static String PROJECT_BASE_PATH = System.getProperty("user.dir");
-	 public final static String propertyfile_path  = PROJECT_BASE_PATH + "/src/test/resources/PetSwaggerProperties.properties";
-	 public final static String petpost = PROJECT_BASE_PATH + "/src/test/resources/petadd.json";
-	    public final static String updatepet = PROJECT_BASE_PATH + "/src/test/resources/updatepet.json";
-	    public final static String uploadimage = PROJECT_BASE_PATH + "/src/test/resources/puppy.jpg";
-	    public final static int statuscode=200;
+	public final static String propertyfile_path  = PROJECT_BASE_PATH + "/src/test/resources/PetSwaggerProperties.properties";
+	public final static String petpost = PROJECT_BASE_PATH + "/src/test/resources/petadd.json";
+	public final static String updatepet = PROJECT_BASE_PATH + "/src/test/resources/updatepet.json";
+	public final static String uploadimage = PROJECT_BASE_PATH + "/src/test/resources/puppy.jpg";
+	public final static int statuscode=200;
+	public final static String pet="/pet/";
+	public final static String uploadimage="/uploadImage";
 }
